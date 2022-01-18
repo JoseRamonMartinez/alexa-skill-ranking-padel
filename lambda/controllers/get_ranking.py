8bddde1104539c9f6ac88e39ddb2978c8d702fc3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-01-18 16:42:45 +0000

    updated lambda/controllers/get_ranking.py.

--- lambda/controllers/get_ranking.py
+++ lambda/controllers/get_ranking.py
@@ -1,7 +1,7 @@
 import boto3
 import json
 
-lambda_client = boto3.client('lambda')
+lambda_client = boto3.client('lambda', region_name='eu-west-1')
 
 def get_ranking():
     invoke_response = lambda_client.invoke(FunctionName="padel-prod-get_ranking",
