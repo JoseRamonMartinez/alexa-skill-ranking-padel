be44c069d7f5d0ec32e05a370522c48edfa87343
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-01-18 16:24:29 +0000

    updated lambda/controllers/get_player_by_name.py.

--- lambda/controllers/get_player_by_name.py
+++ lambda/controllers/get_player_by_name.py
@@ -6,7 +6,7 @@ lambda_client = boto3_client('lambda')
 
 def get_player_by_name(data):
     msg = {"name":data["name"]}
-    invoke_response = lambda_client.invoke(FunctionName="get_player_by_name",
+    invoke_response = lambda_client.invoke(FunctionName="padel-prod-get_player_by_name",
                                            InvocationType='Event',
                                            Payload=json.dumps(msg))
     return invoke_response
