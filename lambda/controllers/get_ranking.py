a94926bbc07cc050a0b37c3ce5d741d637ab99e9
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-01-18 13:16:54 +0000

    updated lambda/controllers/get_ranking.py.

--- lambda/controllers/get_ranking.py
+++ lambda/controllers/get_ranking.py
@@ -5,8 +5,7 @@ import json
 lambda_client = boto3_client('lambda')
 
 def get_ranking():
-    msg = {}
     invoke_response = lambda_client.invoke(FunctionName="get_ranking",
                                            InvocationType='Event',
-                                           Payload=json.dumps(msg))
+                                           Payload=json.dumps({}))
     print(invoke_response)
