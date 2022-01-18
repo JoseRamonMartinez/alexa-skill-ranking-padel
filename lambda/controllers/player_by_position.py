8a18e3cb8c24cf08baff85dd15841e1c19b49485
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-01-18 12:32:54 +0000

    updated lambda/controllers/player_by_position.py.

---
+++ lambda/controllers/player_by_position.py
@@ -0,0 +1,12 @@
+from boto3 import client as boto3_client
+from datetime import datetime
+import json
+
+lambda_client = boto3_client('lambda')
+
+def lambda_handler(event, context):
+    msg = {"position":"1"}
+    invoke_response = lambda_client.invoke(FunctionName="get_player_by_ranking",
+                                           InvocationType='Event',
+                                           Payload=json.dumps(msg))
+    print(invoke_response)
