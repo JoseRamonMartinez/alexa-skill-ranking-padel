2f130a37faa98c604a0c2a8cf68d2e1cd5b3fc52
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-01-19 12:09:57 +0000

    updated lambda/http.py.

--- lambda/http.py
+++ lambda/http.py
@@ -1,11 +1,16 @@
 import requests
+from dotenv import load_dotenv
 
-def http(url, api_key, payload):
-    
+def http(path,payload):
+    load_dotenv()
+
+    api_key = os.environ['API_KEY']
+    aws_domain = os.environ['AWS_URL']
+
     headers = {
     'x-api-key': api_key
     }
-
+    url = f'{}{}'.format(aws_domain, path)
     response = requests.request("GET", url, headers=headers, data=payload)
 
     return response.text
