e92939b2fb5ea26f3f11b9dc5104614000bfb31f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-01-20 13:50:06 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -67,10 +67,11 @@ class PlayRankingHandler(AbstractRequestHandler):
     def handle(self,handler_input):
         language_prompts = handler_input.attributes_manager.request_attributes["_"]
         skill_name = language_prompts["SKILL_NAME"]
-        number = handler_input.request_envelope.request.intent.slots["number"].slot_value.value if handler_input.request_envelope.request.intent.slots["number"].slot_value.value else 3
+        number = handler_input.request_envelope.request.intent.slots["number"].slot_value.value
+        number_filter = int(number) if number else 3
         ranking_list = json.loads(http('/prod/players/ranking',{}))
-        speech_output = random.choice(language_prompts["TOP_RANKING"]).format(number)+'\r\n'
-        sorted_ranking_list = sorted(ast.literal_eval(ranking_list), key=lambda k: k['position'], reverse=False)[0:number]
+        speech_output = random.choice(language_prompts["TOP_RANKING"]).format(number_filter)+'\r\n'
+        sorted_ranking_list = sorted(ast.literal_eval(ranking_list), key=lambda k: k['position'], reverse=False)[0:number_filter]
         speech_output+=f'{sorted_ranking_list[0]["name"].replace("-", " ").title()}'
         for player in sorted_ranking_list[1:(len(sorted_ranking_list)-1)]:
             player_name = player["name"].replace("-", " ").title()
