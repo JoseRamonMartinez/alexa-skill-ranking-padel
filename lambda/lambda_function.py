6a2901b9736b5639abdf9f428d0e4875a6d035f1
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-01-21 11:08:09 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -122,7 +122,7 @@ class PlayPlayerByPositionHandler(AbstractRequestHandler):
         number = int(handler_input.request_envelope.request.intent.slots["number"].slot_value.value)
         ranking_list = json.loads(http('/prod/players/ranking/{}'.format(number)))
         len_ranking_list = len(ast.literal_eval(ranking_list))
-        speech_output = '{}'.format(len(ranking_list)) #'{language_prompts["TOP_PLAYER"][len_ranking_list-1]}'.format(number)
+        speech_output = '{language_prompts["TOP_PLAYER"][len_ranking_list-1]}'.format(number)
 
         #for player in ranking_list:
         #    speech_output+=player["name"].replace("-", " ").title()
