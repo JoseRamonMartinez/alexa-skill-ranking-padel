e9ec301d099b1528ff7423c8fa16ef8ee44aee22
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-01-21 11:31:52 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -128,7 +128,7 @@ class PlayPlayerByPositionHandler(AbstractRequestHandler):
             speech_output += ' y ' if (len(ranking_list)>1) and (player == ranking_list[0]) else ""
         
         if len(ranking_list) == 0:
-            speech_output = random.choice(language_prompts["TOP_PLAYER"]).format(number, number-1)
+            speech_output = random.choice(language_prompts["DRAW"]).format(number, number-1)
 
         reprompt = random.choice(language_prompts["ASK_MORE"])
         
