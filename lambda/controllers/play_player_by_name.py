00ae20d68e6d5e1d96e12ef89c05dd000c2ae47b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-02-01 18:15:57 +0000

    updated lambda/controllers/play_player_by_name.py.

--- lambda/controllers/play_player_by_name.py
+++ lambda/controllers/play_player_by_name.py
@@ -1,11 +1,12 @@
 import json
 import ast
 import random
+import unidecode
 from api.http import http
 
 def play_player_by_name(language_prompts, name):
 
-    name_formatted = name.lower().replace(" ", "-")
+    name_formatted = unidecode.unidecode(name.lower().replace(" ", "-"))
     player_list = ast.literal_eval(json.loads(http('/prod/players/name/{}'.format(name_formatted))))
     if len(player_list) == 0:
         speech_output = random.choice(language_prompts["PLAYER_NO_EXIST"]).format(name)
